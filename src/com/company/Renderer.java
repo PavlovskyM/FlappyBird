ac8997a64bebc1ab5737cafb4e0cc31175cdab03
PavlovskyM <pavlovskis.m@gmail.com>
2022-04-01 19:13:07 +0300

    Initial commit

---
+++ src/com/company/Renderer.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Renderer extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        FlappyBird.flappyBird.repaint(g);
+    }
+}
